9e4681092ccf127335e9acc8a4eea64683072541
Alexander <lebedevskiy_s@rambler.ru>
2020-10-06 11:33:51 +0300

    Make some template

--- Lab_2.2/main.asm
+++ Lab_2.2/main.asm
@@ -1,9 +1,14 @@
 
 section .data
+k dd 1
+l dd 1
+m dd 2
+n dd 2
 
 global _start
 global main
 global CMAIN
+
 section .bss
 
 section .text
@@ -11,10 +16,33 @@ section .text
     mov ebp, esp; for correct debugging
   CMAIN:
   _start:
+  mov eax, [k]
+  mov ebx, [m]
+  cmp eax, ebx
+  je true
+
+  mov eax, [l]
+  mov ebx, [n]
+  cmp eax, ebx
+  je true
+
+  mov eax, [k]
+  sub eax, [m]
+  mov ebx, [l]
+  sub ebx, [n]
+  cmp eax, ebx
+  je true
+
+  false:
+  mov eax, 0
+  jmp exit; end
+
+  true:
+  mov eax, 1
   ; x/4xb &arr
   ; p (int*)(p_end)
   ; p (int*)(&arr)
-
-    mov eax, 1;
-    mov ebx, 0;
-    int 80h;
+  exit:
+  mov eax, 1;
+  mov ebx, 0;
+  int 80h;
