32e44a13572c8bacfd16f956284a188f847407f7
Alexander <lebedevskiy_s@rambler.ru>
2020-10-13 12:57:31 +0300

    Writed working cpp

---
+++ Lab_2.3/main.asm
@@ -0,0 +1,41 @@
+section .data
+n dd 219 ;195
+if_palindrom db 1
+global _start
+global main
+global CMAIN
+
+section .text
+  main:
+  mov ebp, esp; for correct debugging
+  CMAIN:
+  _start:
+
+mov    ebx, DWORD 0x0
+for:
+    cmp    ebx,DWORD 0x3
+    jg     exit
+    mov    eax,0x7
+    sub    eax, ebx
+    ;mov    edx, [n]
+    mov    esi,[n]
+    mov    ecx,eax
+    shr    esi,cl
+    ;mov    eax,  ebx
+    mov    edx, [n]
+    mov    ecx,ebx
+    shr    edx,cl
+    mov    eax,edx
+    xor    eax,esi
+    and    eax,0x1
+      test   eax,eax
+      je     if
+      mov   BYTE [if_palindrom],0x0
+if:
+  add    ebx,0x1
+  jmp    for
+
+exit:
+mov eax, 1;
+movzx ebx, BYTE[if_palindrom]
+int 80h;
---
+++ Lab_2.3/main.cpp
@@ -0,0 +1,25 @@
+#include <math.h>
+int pow (int x, int i)
+{
+	int ans=1;
+	if (i-- !=0)
+	{
+	ans*=x;
+	}
+	return ans;
+}
+int main()
+{
+const unsigned int n=5;
+int sum=0;
+int curr_val=11;
+for(int i=0;i<n;++i)
+{
+	sum+=curr_val;
+	curr_val*=-2;
+}
+int sum1=111;
+sum1-=111;
+sum1=11*(pow(-2, n)-1)/((-2)-1);
+return sum-sum1;
+} 
