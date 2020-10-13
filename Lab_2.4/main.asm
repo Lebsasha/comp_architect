220bd426f32f5da0013a5d1ee6170280de73717b
Alexander <lebedevskiy_s@rambler.ru>
2020-10-13 10:39:47 +0300

    First good launch

--- Lab_2.4/main.asm
+++ Lab_2.4/main.asm
@@ -1,7 +1,7 @@
-
 section .data
-n dd 1219
-
+n dd 219
+if_palindrom dd 1
+offsett dd 1
 global _start
 global main
 global CMAIN
@@ -13,74 +13,44 @@ section .text
   mov ebp, esp; for correct debugging
   CMAIN:
   _start:
+; %use masm
 
-
-mov    DWORD PTR [rbp-0xc],0x4c3
-mov    BYTE PTR [rbp-0x1],0x0
-mov    DWORD PTR [rbp-0x8],0x0
-
-  113b:
-  cmp    DWORD PTR [rbp-0x8],0x3
-  jg     116c
-  mov    eax,DWORD PTR [rbp-0x8]
-  mov    edx,DWORD PTR [rbp-0xc]
-  mov    esi,edx
-  mov    ecx,eax
-  shl    esi,cl
-  mov    eax,DWORD PTR [rbp-0x8]
-  mov    edx,DWORD PTR [rbp-0xc]
-  mov    ecx,eax
-  shr    edx,cl
-  mov    eax,edx
-  xor    eax,esi
-  and    eax,0x1
-  test   eax,eax
-    je     1166
-    mov    BYTE PTR [rbp-0x1],0x1
-
-    1166:
-  add    DWORD PTR [rbp-0x8],0x1
-  jmp    113b
-
-116c:
-movzx  eax,BYTE PTR [rbp-0x1]
-    
-
-
-
-
-
-
-
-
-
-mov    DWORD PTR [rbp-0xc],0xdb
-mov    BYTE PTR [rbp-0x1],0x1
-mov    DWORD PTR [rbp-0x8],0x0
-113b:
-    cmp    DWORD PTR [rbp-0x8],0x3
-    jg     1171
+mov    DWORD [n],0xdb
+mov    DWORD[if_palindrom],0x1
+mov    [ebx],DWORD 0x0
+for:
+    cmp    [ebx],DWORD 0x3
+    jg     exit
     mov    eax,0x7
-    sub    eax,DWORD PTR [rbp-0x8]
-    mov    edx,DWORD PTR [rbp-0xc]
+    sub    eax, [ebx]
+    mov    edx, [n]
     mov    esi,edx
     mov    ecx,eax
     shr    esi,cl
-    mov    eax,DWORD PTR [rbp-0x8]
-    mov    edx,DWORD PTR [rbp-0xc]
+    mov    eax,  [ebx]
+    mov    edx, [n]
     mov    ecx,eax
     shr    edx,cl
     mov    eax,edx
     xor    eax,esi
     and    eax,0x1
     test   eax,eax
-      je     116b
-      mov    BYTE PTR [rbp-0x1],0x0
-116b:
-  add    DWORD PTR [rbp-0x8],0x1
-  jmp    113b
-1171:	
-movzx  eax,BYTE PTR [rbp-0x1]
+      je     if
+      mov   DWORD [if_palindrom],0x0
+if:
+  add    DWORD [ebx],0x1
+  jmp    for
+
+exit:
+mov eax, 1;
+mov  ebx, [if_palindrom]
+int 80h;
+
+
+
+
+
+
   ; false:
   ; mov eax, 0
   ; jmp exit; end
@@ -90,7 +60,7 @@ movzx  eax,BYTE PTR [rbp-0x1]
   ; ; x/4xb &arr
   ; ; p (int*)(p_end)
   ; ; p (int*)(&arr)
-  exit:
-  mov eax, 1;
-  mov ebx, 0;
-  int 80h;
+  ; exit:
+  ; mov eax, 1;
+  ; mov ebx, 0;
+  ; int 80h;
