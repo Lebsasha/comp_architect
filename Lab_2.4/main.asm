e397c41a17f9a14115ce75069bb32c869a455019
Alexander <lebedevskiy_s@rambler.ru>
2020-10-13 12:10:52 +0300

    Make program work

--- Lab_2.4/main.asm
+++ Lab_2.4/main.asm
@@ -1,40 +1,34 @@
 section .data
-n dd 195
+n dd 219;195
 if_palindrom dd 1
-offsett dd 1
 global _start
 global main
 global CMAIN
-
-section .bss
 
 section .text
   main:
   mov ebp, esp; for correct debugging
   CMAIN:
   _start:
-; %use masm
 
-;mov    DWORD [n],0xdb
-mov    DWORD[if_palindrom],0x1
-mov    ebx,DWORD 0x0
+mov    ebx, DWORD 0x0
 for:
     cmp    ebx,DWORD 0x3
     jg     exit
     mov    eax,0x7
     sub    eax, ebx
-    mov    edx, [n]
-    mov    esi,edx
+    ;mov    edx, [n]
+    mov    esi,[n]
     mov    ecx,eax
     shr    esi,cl
-    mov    eax,  ebx
+    ;mov    eax,  ebx
     mov    edx, [n]
-    mov    ecx,eax
+    mov    ecx,ebx
     shr    edx,cl
     mov    eax,edx
     xor    eax,esi
     and    eax,0x1
-    test   eax,eax
+      test   eax,eax
       je     if
       mov   DWORD [if_palindrom],0x0
 if:
