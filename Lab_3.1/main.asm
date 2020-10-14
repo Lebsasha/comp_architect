b5f9e327f2fa6522c61a2bdceb2f28c333eca0ec
Alexander <lebedevskiy_s@rambler.ru>
2020-10-14 11:02:02 +0300

    First good asm result

--- Lab_3.1/main.asm
+++ Lab_3.1/main.asm
@@ -1,9 +1,10 @@
 section .data
 n dd 219 ;195
-sum dd 0;
-x dd 0
-i dd 0
+p dd 0;
 result dd 0
+result_f dd 0
+arr dd 1, 2, 3, 4, 5, 10 ,11, 125, 1125
+size dd 4*9
 global _start
 global main
 global CMAIN
@@ -15,76 +16,70 @@ section .text
   ;_start:
   
 
-
-push   rbp
-;    1126:	48 89 e5             	mov    rbp,rsp
-    1129:	89 7d fc             	mov    DWORD [result],edi
-    112c:	                    	cmp    DWORD [result],0x1
-    1130:	74 49                	je     117b
-    1132:	8b 4d fc             	mov    ecx,DWORD [result]
-    1135:	ba 67 66 66 66       	mov    edx,0x66666667
-    113a:	89 c8                	mov    eax,ecx
-    113c:	f7 ea                	imul   edx
-    113e:	d1 fa                	sar    edx,1
-    1140:	89 c8                	mov    eax,ecx
-    1142:	c1 f8 1f             	sar    eax,0x1f
-    1145:	29 c2                	sub    edx,eax
-    1147:	89 d0                	mov    eax,edx
-    1149:	89 c2                	mov    edx,eax
-    114b:	c1 e2 02             	shl    edx,0x2
-    114e:	01 c2                	add    edx,eax
-    1150:	89 c8                	mov    eax,ecx
-    1152:	29 d0                	sub    eax,edx
-    1154:	85 c0                	test   eax,eax
-    1156:	75 1c                	jne    1174
-    1158:	8b 4d fc             	mov    ecx,DWORD [result]
-    115b:	ba 67 66 66 66       	mov    edx,0x66666667
-    1160:	89 c8                	mov    eax,ecx
-    1162:	f7 ea                	imul   edx
-    1164:	d1 fa                	sar    edx,1
-    1166:	89 c8                	mov    eax,ecx
-    1168:	c1 f8 1f             	sar    eax,0x1f
-    116b:	29 c2                	sub    edx,eax
-    116d:	89 d0                	mov    eax,edx
-    116f:	89 45 fc             	mov    DWORD [result],eax
-    1172:	eb b8                	jmp    112c
-    1174:	                       	mov    eax,0x0
-    1179:	eb 05                	jmp    1180
-    117b:	                    	mov    eax,0x1
-    1180:	                     	pop    rbp
-    1181:	c3                   	ret    
-
 ; 0000000000001182 <main>:
-    ; 1182:	55                   	push   rbp
-    ; 1183:	48 89 e5             	mov    rbp,rsp
-    ; 1186:	48 83 ec 30          	sub    rsp,0x30
-    ; 118a:	c7 45 d0 01 00 00 00 	mov    DWORD [rbp-0x30],0x1
-    ; 1191:	c7 45 d4 02 00 00 00 	mov    DWORD [rbp-0x2c],0x2
-    ; 1198:	c7 45 d8 03 00 00 00 	mov    DWORD [rbp-0x28],0x3
-    ; 119f:	c7 45 dc 04 00 00 00 	mov    DWORD [rbp-0x24],0x4
-    ; 11a6:	c7 45 e0 05 00 00 00 	mov    DWORD [rbp-0x20],0x5
-    11ad:	c7 45 fc 00 00 00 00 	mov    DWORD [result],0x0
-    11b4:	48 8d 45 d0          	lea    eax,[rbp-0x30]
-    11b8:	48 89 45 f0          	mov    DWORD [p],eax
+    ; push   rbp
+    ; mov    rbp,rsp
+    ; sub    rsp,0x30
+    ; mov    DWORD [arr],0x1
+    ; mov    DWORD [rbp-0x2c],0x2
+    ; mov    DWORD [rbp-0x28],0x3
+    ; mov    DWORD [rbp-0x24],0x4
+    ; mov    DWORD [rbp-0x20],0x5
+    mov    DWORD [result],0x0
+    lea    eax,[arr]
+    mov    DWORD [p],eax
 
-    11bc:                           lea    eax,[rbp-0x30]
-    11c0:	48 83 c0 14          	add    eax,0x14
-    11c4:	48 39 45 f0          	cmp    DWORD [p],eax ;cmp
-    11c8:	73 21                	jae    11eb
-    ; 11ca:	48 8b 45 f0          	mov    eax,DWORD [p]
-    11ce:	8b 00                	mov    eax,DWORD [p]
-    11d0:	89 c7                	mov    edi,eax
-    11d2:	e8 4e ff ff ff       	call   1125
-    11d7:	84 c0                	test   al,al
-    11d9:	74 09                	je     11e4
-    ; 11db:	48 8b 45 f0          	mov    eax,DWORD [p]
-    11df:	8b 00                	mov    eax,DWORD [p]
-    11e1:	01 45 fc             	add    DWORD [result],eax
-    11e4:	                    	add    DWORD [p],0x4
-    11e9:	eb d1                	jmp    11bc
-    11eb:	                     	mov    eax,DWORD [result]
+    for:
+    lea    eax,[arr]
+    add    eax, [size]
+    cmp    DWORD [p],eax ;cmp
+    jae    for_exit
+    ; mov    eax,DWORD [p]
+    mov    eax, DWORD [p]
+    mov    eax, [eax]
+    mov    edi,eax
+    call   pow_5
+    test   al,al
+    je     if_not_mod_5
+    ; mov    eax,DWORD [p]
+    mov    eax,DWORD [p]
+    mov    eax, [eax]
+    add    DWORD [result],eax
+    if_not_mod_5:
+    add    DWORD [p],0x4
+    jmp    for
+    for_exit:
+    add DWORD[result], 48
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, result
+    mov edx, 4
+    int    80h
+    mov    eax, 1
+    mov    ebx,DWORD [result]
+    int    80h
 
 
+pow_5:
+    mov    DWORD [result_f],edi
+    while:
+    cmp    DWORD [result_f],0x1
+    je     exit_while
+    mov eax, DWORD [result_f]
+    mov edx, 0
+    mov esi, 5
+    div esi
+    test   edx,edx
+    jne    mod_5_not_0
+    mov    DWORD [result_f],eax
+    jmp    while
+    mod_5_not_0:
+    mov    eax,0x0
+    jmp    return
+    exit_while:
+    mov    eax,0x1
+    return:
+    ret    
 
 
 
