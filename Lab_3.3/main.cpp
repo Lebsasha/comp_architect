8f9fe385aa6e4bd418551bf10b570c625cd4e324
Alexander <lebedevskiy_s@rambler.ru>
2020-10-14 16:52:02 +0300

    First cpp good result

--- Lab_3.3/main.cpp
+++ Lab_3.3/main.cpp
@@ -12,14 +12,43 @@ bool if_5_pow (int x)
 	}
 	return true;
 }
+#include <iostream>
+#include <iomanip>
+/**
+ * @result Prints in matrix Nstr × Nstb form numbers.
+ */
+template<typename T>
+void View (T* pArray, size_t Nstr, const size_t Nstb) noexcept
+{
+    while (Nstr--)
+    {
+        for (size_t j = 0; j < Nstb-1; ++j)
+        {
+            std::cout<<std::setw(3)<<(*pArray)<<" ";
+            pArray++;
+        }
+        std::cout<<std::setw(3)<<*pArray++;
+        std::cout<<std::endl;
+    }
+    std::cout<<std::endl;
+}
 int main()
 {
-int arr[]={1,2,3,4,5};
-int sum=0;
-for(int* p=arr;p<arr+5;++p)
+int A[]={1,2,3, 4,5,6, 7,8,8};
+int C[9]={0};
+int n = sizeof(A)/sizeof(int)-6;
+for (int i=0; i < n; ++i)
+for (int k=0; k < n; ++k)
+for (int j=0; j < n; ++j)
 {
-	if (if_5_pow(*p))
-		sum+=*p;
+    C[i*n+k]+=A[i*n+j]*A[k*n+j];
 }
-return sum;
-} 
+View(A, 3, 3);
+View(C, 3, 3);
+//for(int* p=A;p<A+5;++p)
+//{
+//	if (if_5_pow(*p))
+//		sum+=*p;
+//}
+return 0;
+}
