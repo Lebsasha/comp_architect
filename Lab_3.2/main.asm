a68bcf4548067cd3f86b68c269b93cb7cd4901d3
Alexander <lebedevskiy_s@rambler.ru>
2020-10-14 15:42:40 +0300

    Refactored code

--- Lab_3.2/main.asm
+++ Lab_3.2/main.asm
@@ -1,7 +1,6 @@
 section .data
 arr dd 1, 2, 3, 4, 5, -4, -6, -1, 0, 8, 10 ,11, 125, 1125, 3125
 size dd 4*15
-result dd 0
 
 global _start
 global main
@@ -9,18 +8,17 @@ global CMAIN
 
 section .bss
 n_arr resd 15; resb 4*5
-result_f resd 1
 p resd 1
 n_p resd 1
 p_end resd 1
 n_p_end resd 1
+
 section .text
   main:
   mov ebp, esp; for correct debugging
   CMAIN:
- ; _start:
+  _start:
   
-
     lea    eax, [arr]
     mov    DWORD [p],eax
     add eax, [size]
@@ -65,58 +63,6 @@ section .text
       jmp    for_pos
     for_exit_pos:
 
-
     mov    eax, 1
     mov    ebx,0
     int    80h
-
-
-    ; add DWORD[result], 48
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, result
-    mov edx, 4
-    int    80h
-    sub DWORD[result], 48
-    mov    eax, 1
-    mov    ebx,DWORD [result]
-
-
-find_min_1:
-    mov    DWORD [result_f],edi
-    for_f:
-    cmp    ecx,ebx
-    jae    for_f_exit
-      cmp eax, [ecx]
-      jle min
-      cmp eax, 0
-      jle min
-      MOV eax, [ecx]
-      min:
-      add    ecx,0x4
-      jmp    for_f
-
-    for_f_exit:
-    ret
-
-    while:
-    cmp    DWORD [result_f],0x1
-    je     exit_while
-        mov eax, DWORD [result_f]
-        mov edx, 0
-        mov esi, 5
-        div esi
-        test   edx,edx
-        jne    mod_5_not_0
-            mov    DWORD [result_f],eax
-        jmp    while
-
-
-
-    mod_5_not_0:
-    mov    eax,0x0
-    jmp    return
-    exit_while:
-    mov    eax,0x1
-    return:
-    ret    
