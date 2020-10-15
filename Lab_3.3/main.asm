40c9a8f6fb1e8ce32a0c285229daf453a9ed54de
Alexander <lebedevskiy_s@rambler.ru>
2020-10-15 18:27:51 +0300

    First good result

--- Lab_3.3/main.asm
+++ Lab_3.3/main.asm
@@ -1,14 +1,18 @@
 section .data
 arr dd 1, 2, 3,  4, 5, 6,  7, 8, 8
-size dd 4*5
+size dd 3
 result dd 0
-
 global _start
 global main
 global CMAIN
 
 section .bss
 n_arr resd 9; resb 9*4
+
+i resd 1
+j resd 1
+k resd 1
+
 p resd 1
 result_f resd 1
 
@@ -16,89 +20,84 @@ section .text
   main:
   mov ebp, esp; for correct debugging
   CMAIN:
- ; _start:
+;  _start:
   
 
-push   rbp
-    mov    rbp,rsp
-    mov    DWORD PTR [i0],0x1
-    mov    DWORD PTR [rbp-0x3c],0x2
-    mov    DWORD PTR [rbp-0x38],0x3
-    mov    DWORD PTR [rbp-0x34],0x4
-    mov    DWORD PTR [rbp-0x30],0x5
-    mov    DWORD PTR [rbp-0x2c],0x6
-    mov    DWORD PTR [rbp-0x28],0x7
-    mov    DWORD PTR [rbp-0x24],0x8
-    mov    DWORD PTR [rbp-0x20],0x8
 
-    mov    QWORD PTR [rbp-0x70],0x0
-    mov    QWORD PTR [rbp-0x68],0x0
-    mov    QWORD PTR [rbp-0x60],0x0
-    mov    QWORD PTR [rbp-0x58],0x0
-    mov    DWORD PTR [rbp-0x50],0x0
-    mov    DWORD PTR [size],0x3 -- size
-    mov    DWORD PTR [i],0x0
+    ; mov    DWORD [i0],0x1
+    ; mov    DWORD [rbp-0x3c],0x2
+    ; mov    DWORD [rbp-0x38],0x3
+    ; mov    DWORD [rbp-0x34],0x4
+    ; mov    DWORD [rbp-0x30],0x5
+    ; mov    DWORD [rbp-0x2c],0x6
+    ; mov    DWORD [rbp-0x28],0x7
+    ; mov    DWORD [rbp-0x24],0x8
+    ; mov    DWORD [rbp-0x20],0x8
 
-    for_i:	mov    eax,DWORD PTR [i]
-    cmp    eax,DWORD PTR [size]
+    ; mov    QWORD [rbp-0x70],0x0
+    ; mov    QWORD [rbp-0x68],0x0
+    ; mov    QWORD [rbp-0x60],0x0
+    ; mov    QWORD [rbp-0x58],0x0
+    ; mov    DWORD [rbp-0x50],0x0
+    mov    DWORD [size],0x3
+    mov    DWORD [i],0x0
+
+    for_i:	mov    eax,DWORD [i]
+    cmp    eax,DWORD [size]
     jge    exit_for_i
-    mov    DWORD PTR [k],0x0
-    for_k: mov    eax,DWORD PTR [k]
-    cmp    eax,DWORD PTR [size]
+    mov    DWORD [k],0x0
+    for_k: mov    eax,DWORD [k]
+    cmp    eax,DWORD [size]
     jge    exit_for_k
-    mov    DWORD PTR [j],0x0
-    for_j: mov    eax,DWORD PTR [j]
-    cmp    eax,DWORD PTR [size]
+    mov    DWORD [j],0x0
+    for_j: mov    eax,DWORD [j]
+    cmp    eax,DWORD [size]
     jge    exit_for_j
-        mov    eax,DWORD PTR [i]
-        imul   eax,DWORD PTR [size]
+        mov    eax,DWORD [i]
+        imul   eax,DWORD [size]
         mov    edx,eax
-        mov    eax,DWORD PTR [k]
+        mov    eax,DWORD [k]
         add    eax,edx
         ; cdqe   
-        mov    ecx,DWORD PTR [n_arr+ eax*4] ; +=
-        mov    eax,DWORD PTR [i]
-        imul   eax,DWORD PTR [size]
+        mov    ecx,DWORD [n_arr+ eax*4] ; +=
+        mov    eax,DWORD [i]
+        imul   eax,DWORD [size]
         mov    edx,eax
-        mov    eax,DWORD PTR [j]
+        mov    eax,DWORD [j]
         add    eax,edx
         ;cdqe   
-        mov    edx,DWORD PTR [arr+eax*4] ; a[k] [j]
-        mov    eax,DWORD PTR [k]
-        imul   eax,DWORD PTR [size]
+        mov    edx,DWORD [arr+eax*4] ; a[k] [j]
+        mov    eax,DWORD [k]
+        imul   eax,DWORD [size]
         mov    esi,eax
-        mov    eax,DWORD PTR [j]
+        mov    eax,DWORD [j]
         add    eax,esi
         ;cdqe   
-        mov    eax,DWORD PTR [arr+eax*4] ;a[i][k]
+        mov    eax,DWORD [arr+eax*4] ;a[i][k]
         imul   edx,eax ; a[k] [j]*a[i][k]
-        mov    eax,DWORD PTR [i]
-        imul   eax,DWORD PTR [size]
+        mov    eax,DWORD [i]
+        imul   eax,DWORD [size]
         mov    esi,eax
-        mov    eax,DWORD PTR [k]
+        mov    eax,DWORD [k]
         add    eax,esi
         add    edx,ecx;+=a[k] [j]*a[i][k]
         ;cdqe   
-        mov    DWORD PTR [n_arr+eax*4-0x70],edx
-        add    DWORD PTR [j],0x1
+        mov    DWORD [n_arr+eax*4],edx
+        add    DWORD [j],0x1
     jmp    for_j
     exit_for_j:
-        add    DWORD PTR [k],0x1
+        add    DWORD [k],0x1
     jmp    for_k
     exit_for_k:
-        add    DWORD PTR [i],0x1
+        add    DWORD [i],0x1
     jmp    for_i
     exit_for_i:
-
-    mov    eax,0x0
-    mov    DWORD PTR [rbp-0x14],0xffff
-    pop    rbp
-    ret
 
     exit:
     mov    eax, 1
     ; mov    ebx, 0
     int    80h
+
     
 
 
