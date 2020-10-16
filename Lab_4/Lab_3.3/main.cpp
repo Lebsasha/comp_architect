a715e5c11a1805bd46de5ec20682359fcb3218bc
Alexander <lebedevskiy_s@rambler.ru>
2020-10-16 22:49:34 +0300

    From book (bad)

--- Lab_4/Lab_3.3/main.cpp
+++ Lab_4/Lab_3.3/main.cpp
@@ -1,31 +1,8 @@
 #include <iostream>
-#include <iomanip>
-/**
- * @result Prints in matrix Nstr × Nstb form numbers.
- */
-template<typename T>
-void View (T* pArray, size_t Nstr, const size_t Nstb) noexcept
+using namespace std;
+extern "C" int factorial(int);
+int main(void)
 {
-    while (Nstr--)
-    {
-        for (size_t j = 0; j < Nstb-1; ++j)
-        {
-            std::cout<<std::setw(3)<<(*pArray)<<" ";
-            pArray++;
-        }
-        std::cout<<std::setw(3)<<*pArray++;
-        std::cout<<std::endl;
-    }
-    std::cout<<std::endl;
-}
-extern "C" void multiply_matrix(int* pA, int* pC, int n);
-int main()
-{
-int A[]={1,2,3, 4,5,6, 7,8,8};
-int C[9]={0};
-int n = sizeof(A)/sizeof(int)-6;
-multiply_matrix(A,C,n);
-View(A, 3, 3);
-View(C, 3, 3);
+cout << factorial(5) << endl;
 return 0;
 }
