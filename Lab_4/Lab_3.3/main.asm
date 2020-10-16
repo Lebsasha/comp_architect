82e5b08a880bfed09701ce22ac1bd07a9771b051
Alexander <lebedevskiy_s@rambler.ru>
2020-10-17 00:10:19 +0300

    Returned to the beginnig

--- Lab_4/Lab_3.3/main.asm
+++ Lab_4/Lab_3.3/main.asm
@@ -1,23 +1,88 @@
-SECTION .text ; program
-global factorial ; linux
-global _factorial ; windows
+section .data
+;arr dd 1, 2, 3,  4, 5, 6,  7, 8, 8
+size dd 3
+result dd 0
+global _start
 global main
-factorial:
-_factorial:
-push rbp ; save base pointer
-mov rbp, rsp ; store stack pointer
-push rcx ; save rcx
-;;; start function
-add rbp, 16
-mov rcx, [rbp] ; rcx = first argumrnt
-mov rax, 1 ; rax = 1
-mainloop:
-cmp rcx, 0 ; if(rcx == 0)
-jz done ; goto done
-mul rcx ; else rax = rax * rcx
-dec rcx ; rcx = rcx - 1
-jmp mainloop
-done:
-pop rcx ; restore rcx
-pop rbp ; restore rbp
-ret ; return from function
+global CMAIN
+global multiply_matrix
+
+section .bss
+n_arr resd 2 ;9 resb 9*4
+arr resd 2
+i resd 1
+j resd 1
+k resd 1
+
+p resd 1
+result_f resd 1
+
+section .text
+  ;main:
+  mov ebp, esp; for correct debugging
+  ;CMAIN:
+;  _start:
+  
+
+multiply_matrix:
+    push rbp
+    mov rbp, rsp; for correct debugging
+    ;mov rax, [rbp+16]
+    ;mov QWORD[arr], rax
+    ;mov    DWORD [size],0x3
+    mov DWORD[arr],s
+    mov    DWORD [i],0x0
+
+    for_i:	mov    eax,DWORD [i]
+    cmp    eax,DWORD [size]
+    jge    exit_for_i
+    mov    DWORD [k],0x0
+    for_k: mov    eax,DWORD [k]
+    cmp    eax,DWORD [size]
+    jge    exit_for_k
+    mov    DWORD [j],0x0
+    for_j: mov    eax,DWORD [j]
+    cmp    eax,DWORD [size]
+    jge    exit_for_j
+        mov    eax,DWORD [i]
+        imul   eax,DWORD [size]
+        mov    edx,eax
+        mov    eax,DWORD [k]
+        add    eax,edx
+        ; cdqe   
+        mov    ecx,DWORD [n_arr+ eax*4] ; +=
+        mov    eax,DWORD [i]
+        imul   eax,DWORD [size]
+        mov    edx,eax
+        mov    eax,DWORD [j]
+        add    eax,edx
+        ;cdqe   
+        mov    edx,DWORD [arr+eax*4] ; a[k] [j]
+        mov    eax,DWORD [k]
+        imul   eax,DWORD [size]
+        mov    esi,eax
+        mov    eax,DWORD [j]
+        add    eax,esi
+        ;cdqe   
+        mov    eax,DWORD [arr+eax*4] ;a[i][k]
+        imul   edx,eax ; a[k] [j]*a[i][k]
+        mov    eax,DWORD [i]
+        imul   eax,DWORD [size]
+        mov    esi,eax
+        mov    eax,DWORD [k]
+        add    eax,esi
+        add    edx,ecx;+=a[k] [j]*a[i][k]
+        ;cdqe   
+        mov    DWORD [n_arr+eax*4],edx
+        add    DWORD [j],0x1
+    jmp    for_j
+    exit_for_j:
+        add    DWORD [k],0x1
+    jmp    for_k
+    exit_for_k:
+        add    DWORD [i],0x1
+    jmp    for_i
+    exit_for_i:
+
+    pop rbp
+    ret
--- Lab_4/Lab_3.3/main.cpp
+++ Lab_4/Lab_3.3/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
-using namespace std;
-extern "C" int factorial(int);
-int main(void)
+#include <iomanip>
+/**
+ * @result Prints in matrix Nstr × Nstb form numbers.
+ */
+template<typename T>
+void View (T* pArray, size_t Nstr, const size_t Nstb) noexcept
 {
-cout << factorial(5) << endl;
+    while (Nstr--)
+    {
+        for (size_t j = 0; j < Nstb-1; ++j)
+        {
+            std::cout<<std::setw(3)<<(*pArray)<<" ";
+            pArray++;
+        }
+        std::cout<<std::setw(3)<<*pArray++;
+        std::cout<<std::endl;
+    }
+    std::cout<<std::endl;
+}
+extern "C" void multiply_matrix(int* pA, int* pC, int n);
+int main()
+{
+int A[]={1,2,3, 4,5,6, 7,8,8};
+int C[9]={0};
+int n = sizeof(A)/sizeof(int)-6;
+multiply_matrix(A,C,n);
+View(A, 3, 3);
+View(C, 3, 3);
 return 0;
 }
