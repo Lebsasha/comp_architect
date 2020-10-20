477e1699cc9b50dcf3cae83aa84f5f1f57012bc7
Alexander <lebedevskiy_s@rambler.ru>
2020-10-20 11:36:08 +0300

    Remove unused code in second part

--- Lab_4/Lab_3.3/main.asm
+++ Lab_4/Lab_3.3/main.asm
@@ -1,15 +1,10 @@
 section .data
-;arr dd 1, 2, 3,  4, 5, 6,  7, 8, 8
-size dd 3
-result dd 0
-global _start
-global main
-global CMAIN
 global multiply_matrix
 
 section .bss
-n_arr resd 2 ;9 resb 9*4
+n_arr resd 2
 arr resd 2
+size resd 1
 i resd 1
 j resd 1
 k resd 1
@@ -18,18 +13,10 @@ p resd 1
 result_f resd 1
 
 section .text
-  ;main:
-  mov ebp, esp; for correct debugging
-  ;CMAIN:
-;  _start:
-  
 
 multiply_matrix:
     push rbp
     mov rbp, rsp; for correct debugging
-    ;mov rax, [rbp+16]
-    ;mov QWORD[arr], rax
-    ;mov    DWORD [size],0x3
     mov QWORD[arr], rdi
     mov QWORD[n_arr], rsi
     mov DWORD[size], edx
@@ -52,47 +39,39 @@ multiply_matrix:
         mov    edx,eax
         mov    eax,DWORD [k]
         add    eax,edx
-        ; cdqe   
-        lea    rax, [rax*4]
         mov    rcx,QWORD [n_arr]
-        add    rcx, rax
+        lea    rcx, [rcx+rax*4]
         mov ecx, DWORD[rcx] ; +=
-        ; mov    ecx,DWORD [n_arr+ eax*4] ; +=
         mov    eax,DWORD [i]
         imul   eax,DWORD [size]
         mov    edx,eax
         mov    eax,DWORD [j]
         add    eax,edx
         ;cdqe   
-        ;cwde                                           ; TODO
-        lea    rax, [rax*4]
+        ;cwde -- maybe same as cdqe in 32 bits mode
         mov    rdx,QWORD [arr]
-        add    rdx, rax
-        mov edx, DWORD[rdx] 
-        ; mov    edx,DWORD [arr+eax*4] ; a[k] [j]
+        lea    rdx, [rdx+rax*4]
+        mov edx, DWORD[rdx]; a[i][j]
         mov    eax,DWORD [k]
         imul   eax,DWORD [size]
         mov    esi,eax
         mov    eax,DWORD [j]
         add    eax,esi
-        ;cdqe   
-        lea    rax, [rax*4]
+        ;cdqe
         mov    rsi,QWORD [arr]
-        add    rsi, rax
-        mov eax, DWORD[rsi]  ;a[i][k]
-        ; mov    eax,DWORD [arr+eax*4] ;a[i][k]
-        imul   edx,eax ; a[k] [j]*a[i][k]
+        lea    rsi, [rsi+rax*4]
+        mov eax, DWORD[rsi]  ;a[k][j]
+        imul   edx,eax ; a[i][j]*a[k][j]
         mov    eax,DWORD [i]
         imul   eax,DWORD [size]
         mov    esi,eax
         mov    eax,DWORD [k]
         add    eax,esi
-        add    edx,ecx;+=a[k] [j]*a[i][k]
-        ;cdqe   
-        lea    rax, [rax*4]
+        add    edx,ecx;+=a[i][j]*a[k][j]
+        ;cdqe   ; for extension eax -> rax with zeroing (in fact copying leading sign)
         mov    rsi,QWORD [n_arr]
-        add    rsi, rax
-        mov    DWORD [rsi],edx
+        lea    rsi, [rsi+rax*4]
+        mov    DWORD [rsi],edx; a[i][k]= edx
         add    DWORD [j],0x1
     jmp    for_j
     exit_for_j:
